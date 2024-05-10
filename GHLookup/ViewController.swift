017d465e84bb86aa17007ca4202abe90a66def56
jjcode22 <jjmcoding31@gmail.com>
2024-05-10 18:29:55 +0530

    Initial setup

---
+++ GHLookup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHLookup
+//
+//  Created by JJ on 10/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
